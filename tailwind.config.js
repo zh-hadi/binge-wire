cca8a66bd8d5ac933300eb3ecab0026c3259e47e
zh-hadi <zhhadi50@gmail.com>
2024-11-14 18:45:13 +0600

    this project 5 5 page desing done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'whitec': '#FFFFFF',
+        'redc': '#F50606',
+        'black': "#000000",
+        'bg-main': "#272626"
+      }
+    },
+  },
+  plugins: [],
+}
